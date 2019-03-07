c6e5f7971323b94421085fd04e28433d27170b02
Victor Ruiz <victormruiz92@gmail.com>
2019-03-07 20:45:24 +0100

    he cambiado un poco el enunciado y he hecho hasta el 4

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Contar información: Contar cuantos convenios hay en total.
 
 Buscar o filtrar información: Pedir una fecha de aprobacion y muestra el numero de inscripción de los convenios con esa fecha.
 
-Buscar información relacionada: pedir por teclado un area y mostrar todos los convenios de dicho area.
+Buscar información relacionada: pide por teclado un numero de inscripción y muestra las entidades a las que afecta.
 
-Ejercicio libre: pide por teclado un numero de inscripción y muestra las entidades a las que afecta.
+Ejercicio libre: pedir por teclado un area y mostrar todos los convenios de dicho area.
--- ejercicio.py
+++ ejercicio.py
@@ -16,7 +16,19 @@ while opcion >= 0:
 		for convenio in datos["convenios"]:
 			print("el convenio con numero de expediente",convenio["numeroexpediente"],convenio["vigente"],"está vigente")
 	elif opcion == 2:
-		print("El número total de convenios es",len(datos["convenios"])
+		print("El número total de convenios es",len(datos["convenios"]))
+	elif opcion == 3:
+		fecha=input("Escribe una posible fecha de aprobacion (ej: 28/12/2010): ")
+		for convenio in datos["convenios"]:
+			if convenio["fechaaprobacion"] == fecha:
+				print("el numero de inscripcion asociado con esa fecha es",convenio["numeroinscripcion"])
+	elif opcion == 4:
+		numinsc=input("Escribe un número de inscripción: ")
+		print("Las entidades que corresponden a ese numero de inscripción son:")
+		for convenio in datos["convenios"]:
+			if convenio["numeroinscripcion"] == numinsc:
+				print(convenio["entidades"][0]["entidad"])
+
 	print("Elige una opción: ")
 	print("1.- ejercicio 1: ")
 	print("2.- ejercicio 2: ")
